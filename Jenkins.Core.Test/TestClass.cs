da3b7456cf40552cdc66daf2d2db4ed43762c8e4
604572782@qq.com <l234567890j>
2019-01-29 10:25:28 +0800

    git持续集成测试

---
+++ Jenkins.Core.Test/TestClass.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jenkins.Core.Test
+{
+	[TestFixture]
+	public class TestClass
+	{
+		[Test]
+		public void TestHelloWorld()
+		{
+			Assert.Pass(HelloWolrdHelper.GetString());
+        }
+        [Test]
+        public void TestSucceeded()
+        {
+            Assert.IsTrue(true);
+        }
+        [Test]
+        public void TestSucceeded2()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
